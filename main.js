529e070240f3c32b2e09e2f7348c0a6ac8d874dd
Jonas Holbech <jh@jonasholbech.dk>
2020-04-15 13:09:32 +0200

    controlled

--- main.js
+++ main.js
@@ -4,4 +4,14 @@ const form = document.querySelector("form");
 window.form = form;
 const elements = form.elements;
 window.elements = elements;
-elements.enemies.value = 12;
+
+elements.unknown.addEventListener("click", (e) => {
+  elements.dob.disabled = !elements.dob.disabled;
+  setTimeout(() => {
+    elements.unknown.click();
+  }, 1000);
+});
+
+elements.real_name.addEventListener("click", (e) => {
+  elements.real_name.blur();
+});
