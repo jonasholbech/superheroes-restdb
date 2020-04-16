db5cbc7ab7fde2467f21f2c1ee4344cdd4e0bd35
Jonas Holbech <jh@jonasholbech.dk>
2020-04-16 21:45:58 +0200

    getting as well

--- main.js
+++ main.js
@@ -1,63 +1,101 @@
 import { endpoint, apiKey } from "./modules/settings";
 
-const form = document.querySelector("form");
-window.form = form;
-const elements = form.elements;
-window.elements = elements;
+window.addEventListener("load", init);
 
-elements.unknown.addEventListener("click", (e) => {
-  elements.dob.disabled = !elements.dob.disabled;
-});
+function init() {
+  setupForm();
+  getSuperheroes();
+}
+function setupForm() {
+  const form = document.querySelector("form");
+  window.form = form;
+  const elements = form.elements;
+  window.elements = elements;
 
-form.setAttribute("novalidate", true);
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let validForm = true;
-  const formElements = form.querySelectorAll("input");
-  formElements.forEach((el) => {
-    el.classList.remove("invalid");
+  elements.unknown.addEventListener("click", (e) => {
+    elements.dob.disabled = !elements.dob.disabled;
   });
 
-  const cbs = [...form.querySelectorAll(`[name=powers]`)];
-  const checked = cbs.filter((el) => el.checked);
-
-  const errorContainerPowers = form.querySelector("fieldset p");
-  if (checked.length === 0) {
-    validForm = false;
-    errorContainerPowers.classList.remove("hidden");
-  } else {
-    errorContainerPowers.classList.add("hidden");
-  }
-  if (form.checkValidity() && validForm) {
-    //send to restdb/api
-    console.log("SUBMIT READY");
-  } else {
-    //!awesome
+  form.setAttribute("novalidate", true);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let validForm = true;
+    const formElements = form.querySelectorAll("input");
     formElements.forEach((el) => {
-      if (!el.checkValidity()) {
-        el.classList.add("invalid");
-      }
+      el.classList.remove("invalid");
     });
-  }
-});
 
-/*
-1. remove all error messages
-2. loop through 
-3. show error messages
-  4. create with JS
-  5. have them in the DOM hide/show
-  6. 
-  const postData = JSON.stringify(data);
-fetch("someurl", {
-  method: "post",
-  headers: {
-    "Content-Type": "application/json; charset=utf-8",
-    "x-apikey": "your-cors-api-key",
-    "cache-control": "no-cache"
-  },
-  body: postData
-})
-  .then(res => res.json())
-  .then(data => console.log(data));
-*/
+    const cbs = [...form.querySelectorAll(`[name=powers]`)];
+    const checked = cbs.filter((el) => el.checked);
+
+    const errorContainerPowers = form.querySelector("fieldset p");
+    if (checked.length === 0) {
+      validForm = false;
+      errorContainerPowers.classList.remove("hidden");
+    } else {
+      errorContainerPowers.classList.add("hidden");
+    }
+    if (form.checkValidity() && validForm) {
+      postSuperhero({
+        real_name: form.elements.real_name.value,
+        alias: form.elements.alias.value,
+        dob: form.elements.dob.value, //TODO:
+        enemies: form.elements.enemies.value,
+        powers: checked.map((el) => el.value),
+      });
+      form.reset();
+    } else {
+      //!awesome
+      formElements.forEach((el) => {
+        if (!el.checkValidity()) {
+          el.classList.add("invalid");
+        }
+      });
+    }
+  });
+}
+function postSuperhero(payload) {
+  const postData = JSON.stringify(payload);
+  fetch(endpoint + "rest/superheroes", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": apiKey,
+      "cache-control": "no-cache",
+    },
+    body: postData,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      showSuperhero(data);
+    });
+}
+
+function getSuperheroes() {
+  fetch(endpoint + "rest/superheroes", {
+    method: "get",
+    headers: {
+      accept: "application/json",
+      "x-apikey": apiKey,
+      "cache-control": "no-cache",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => data.forEach(showSuperhero));
+}
+
+const template = document.querySelector("template").content;
+const heroContainer = document.querySelector("#herolist");
+function showSuperhero(hero) {
+  const clone = template.cloneNode(true);
+  clone.querySelector("h2").textContent = hero.alias;
+  clone.querySelector("h3 span").textContent = hero.real_name;
+  const ul = clone.querySelector("ul");
+  hero.powers.forEach((pow) => {
+    const li = document.createElement("li");
+    li.textContent = pow;
+    ul.appendChild(li);
+  });
+  heroContainer.appendChild(clone);
+}
